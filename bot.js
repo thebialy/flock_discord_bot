ffd7f6a9e22be212ef043f37cf842fbd8f361675
thebialy <67696052+thebialy@users.noreply.github.com>
2021-01-05 10:29:42 -0600

    more bot setup

--- bot.js
+++ bot.js
@@ -1,2 +1,10 @@
+require('dotenv').config()
+
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log('Our bot is ready to go')
+})
+
+client.login(process.env.BOT_TOKEN)
